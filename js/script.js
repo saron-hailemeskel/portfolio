42b81191ab849a6ad4a9c3541c0f3f150b7bbe14
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-01-08 12:16:34 +0300

    button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("Portfolio loaded successfully");
+const form = document.getElementById("contact-form");
+const message = document.getElementById("form-message");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const text = document.getElementById("message").value;
+
+  if (name === "" || email === "" || text === "") {
+    message.style.color = "red";
+    message.textContent = "Please fill in all fields.";
+  } else {
+    message.style.color = "green";
+    message.textContent = "Message sent successfully!";
+    form.reset();
+  }
+});
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
